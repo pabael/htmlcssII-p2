bef489cae2c111cf4bb2cd52faa151b48395ffca
[Paloma Balmori Elosegui] <[pbalmorie@uoc.edu]>
2024-11-25 18:56:32 +0100

    poster no responsive

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -12,40 +12,45 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   function cargarContenido(url) {
-      fetch(url)
-          .then(response => response.text())
-          .then(data => {
-              document.getElementById("content").innerHTML = data;
-              window.scrollTo(0, 0);
-          })
-          .catch(error => console.error('Error al cargar el contenido:', error));
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("content").innerHTML = data;
+        window.scrollTo(0, 0);
+      })
+      .catch(error => console.error('Error al cargar el contenido:', error));
   }
 
   //Al inicio cargar página principal
-  cargarContenido("./views/main.html");
+  cargarContenido("./views/home.html");
+  document.getElementById("header").classList.remove("alt-color");
+
 
   //Carga la página que ha sido clicada en el header
   document.querySelectorAll("header nav ul li a").forEach(enlace => {
-      enlace.addEventListener("click", function(event) {
-          document.querySelectorAll("header nav ul li a").forEach(link => {
-              link.classList.remove("active");
-          });
-
-          enlace.classList.add("active");
-          event.preventDefault(); 
-          cargarContenido(enlace.getAttribute("href"));
-          
+    enlace.addEventListener("click", function(event) {
+      document.querySelectorAll("header nav ul li a").forEach(link => {
+        link.classList.remove("active");
       });
+
+      enlace.classList.add("active");
+      document.getElementById("header").classList.add("alt-color");
+
+      event.preventDefault(); 
+      cargarContenido(enlace.getAttribute("href"));
+    });
   });
   
   //Al clicar en el icono de la cabecera, carga la página principal
-  document.querySelector(".title").addEventListener("click", function(event) {
-      document.querySelectorAll("header nav ul li a").forEach(link => {
-          link.classList.remove("active");
-      });
-      event.preventDefault();
-      cargarContenido(event.target.getAttribute("href"));
+  document.getElementById("logo").addEventListener("click", function(event) {
+    document.querySelectorAll("header nav ul li a").forEach(link => {
+      link.classList.remove("active");
     });
+    document.getElementById("header").classList.remove("alt-color");
+
+    event.preventDefault();
+    cargarContenido(event.target.getAttribute("href"));
+  });
 
 });
 
